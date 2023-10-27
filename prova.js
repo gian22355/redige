63be0ac0dc9d6c10030bac4cfafab70312446f2f
gian22355 <149201943+gian22355@users.noreply.github.com>
2023-10-27 21:59:24 +0200

    Create prova.js

---
+++ prova.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Termini e Condizioni di redige.it</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Montserrat', sans-serif;
+            padding: 20px;
+        }
+
+        .terms-section {
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        h1 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        ul {
+            list-style-type: disc;
+            margin-left: 20px;
+            margin-bottom: 20px;
+        }
+
+        address {
+            font-style: normal;
+            margin-top: 20px;
+        }
+
+        .divider {
+            border-top: 2px solid black;
+            margin: 30px 0;
+        }
+
+    </style>
+</head>
+<body>
+    <section class="terms-section">
+        <h1>Termini e Condizioni di redige.it</h1>
+        <p>Questi Termini disciplinano</p>
+        <ul>
+            <li>l’utilizzo di redige.it e</li>
+            <li>qualsiasi altro Accordo o rapporto giuridico con il Titolare</li>
+        </ul>
+        <p>in maniera vincolante. Le espressioni con l’iniziale maiuscola sono definite nella relativa sezione di questo documento.</p>
+        <p>L’Utente è pregato di leggere attentamente questo documento.</p>
+        
+        <div class="divider"></div>
+
+        <p>Redige.it è un servizio di:</p>
+        <address>
+            Pontoni Gianluca - Via Molin Nuovo 11 - Udine 33100<br>
+            Indirizzo email del Titolare: team@redige.it
+        </address>
+    </section>
+</body>
+</html>
